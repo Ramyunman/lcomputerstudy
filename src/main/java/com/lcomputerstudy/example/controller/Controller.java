b014c073bb2e8dea250aae04bbe195c69ca20213
Ramyunman <100078669+Ramyunman@users.noreply.github.com>
2023-03-15 11:50:22 +0900

    230315 part7. taglib 부분 안되고 있다

--- src/main/java/com/lcomputerstudy/example/controller/Controller.java
+++ src/main/java/com/lcomputerstudy/example/controller/Controller.java
@@ -3,16 +3,20 @@ package com.lcomputerstudy.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.lcomputerstudy.example.domain.Board;
 import com.lcomputerstudy.example.domain.User;
 import com.lcomputerstudy.example.service.BoardService;
+import com.lcomputerstudy.example.service.UserService;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 	
+	@Autowired UserService userservice;
 	@Autowired BoardService boardservice;
 	
 	@RequestMapping("/")
@@ -20,6 +24,7 @@ public class Controller {
 		
 		List<Board> list = boardservice.selectBoardList();
 		model.addAttribute("list",list);
+		
 		return "/index";
 	}
 	
@@ -30,7 +35,29 @@ public class Controller {
 	
 	@RequestMapping("/signup")
 	public String signup(User user) {
+		//비밀번호 암호화
+		String encodedPassword = new BCryptPasswordEncoder().encode(user.getPassword());
+		
+		//유저 데이터 세팅
+		user.setPassword(encodedPassword);
+		user.setAccountNonExpired(true);
+		user.setEnabled(true);
+		user.setAccountNonLocked(true);
+		user.setCredentialsNonExpired(true);
+		user.setAuthorities(AuthorityUtils.createAuthorityList("ROLE_USER"));
+		
+		//유저 생성
+		userservice.createUser(user);
+		//유저 권한 생성
+		userservice.createAuthorities(user);
+		
 		return "/login";
 	}
+	
+	@RequestMapping(value="/login")
+	public String beforeLogin(Model model) {
+		return "/login";
+	}
+
 
 }
